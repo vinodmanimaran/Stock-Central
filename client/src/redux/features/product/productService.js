67c95f623c7ecbe99926b717ab5a328afa68b0f0
Vinod Kumar <117673132+vinodmanimaran@users.noreply.github.com>
2023-05-22 04:00:11 +0530

    Update productService.js

--- client/src/redux/features/product/productService.js
+++ client/src/redux/features/product/productService.js
@@ -4,31 +4,36 @@ const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 const API_URL = `${BACKEND_URL}/api/products/`;
 
+// Create a custom Axios instance
+const axiosInstance = axios.create();
+
 // Create New Product
 const createProduct = async (formData) => {
-  const response = await axios.post(API_URL, formData);
+  const response = await axiosInstance.post(API_URL, formData);
   return response.data;
 };
 
 // Get all products
 const getProducts = async () => {
-  const response = await axios.get(API_URL);
+  const response = await axiosInstance.get(API_URL);
   return response.data;
 };
 
 // Delete a Product
 const deleteProduct = async (id) => {
-  const response = await axios.delete(API_URL + id);
+  const response = await axiosInstance.delete(API_URL + id);
   return response.data;
 };
+
 // Get a Product
 const getProduct = async (id) => {
-  const response = await axios.get(API_URL + id);
+  const response = await axiosInstance.get(API_URL + id);
   return response.data;
 };
+
 // Update Product
 const updateProduct = async (id, formData) => {
-  const response = await axios.patch(`${API_URL}${id}`, formData);
+  const response = await axiosInstance.patch(`${API_URL}${id}`, formData);
   return response.data;
 };
 
