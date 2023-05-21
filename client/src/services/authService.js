1a36f01c485fdb624816349caecf0ce13fbdbefa
Vinod Kumar <117673132+vinodmanimaran@users.noreply.github.com>
2023-05-22 04:29:12 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 import { toast } from "react-toastify";
 
-export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
+export const BACKEND_URL = process.env.REACT_APP_BACKEND_URL;
 
 export const validateEmail = (email) => {
   return email.match(
@@ -10,139 +10,149 @@ export const validateEmail = (email) => {
   );
 };
 
-// Create a custom Axios instance
-const axiosInstance = axios.create();
-
-axiosInstance.interceptors.response.use(
-  (response) => {
+// Register User
+export const registerUser = async (userData) => {
+  try {
+    const response = await axios.post(
+      `${BACKEND_URL}/api/users/register`,
+      userData,
+      { withCredentials: true }
+    );
     if (response.statusText === "OK") {
-      toast.success(response.data.message);
+      toast.success("User Registered successfully");
     }
-    return response;
-  },
-  (error) => {
+    return response.data;
+  } catch (error) {
     const message =
       (error.response && error.response.data && error.response.data.message) ||
       error.message ||
       error.toString();
     toast.error(message);
-    return Promise.reject(error);
-  }
-);
-
-// Register User
-export const registerUser = async (userData) => {
-  try {
-    const response = await axiosInstance.post(
-      `${BACKEND_URL}/api/users/register`,
-      userData,
-      { withCredentials: true }
-    );
-    return response.data;
-  } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
   }
 };
 
 // Login User
 export const loginUser = async (userData) => {
   try {
-    const response = await axiosInstance.post(
+    const response = await axios.post(
       `${BACKEND_URL}/api/users/login`,
       userData
     );
+    if (response.statusText === "OK") {
+      toast.success("Login Successful...");
+    }
     return response.data;
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
 
 // Logout User
 export const logoutUser = async () => {
   try {
-    await axiosInstance.get(`${BACKEND_URL}/api/users/logout`);
+    await axios.get(`${BACKEND_URL}/api/users/logout`);
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
 
 // Forgot Password
 export const forgotPassword = async (userData) => {
   try {
-    const response = await axiosInstance.post(
+    const response = await axios.post(
       `${BACKEND_URL}/api/users/forgotpassword`,
       userData
     );
-    return response.data;
+    toast.success(response.data.message);
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
 
 // Reset Password
 export const resetPassword = async (userData, resetToken) => {
   try {
-    const response = await axiosInstance.put(
+    const response = await axios.put(
       `${BACKEND_URL}/api/users/resetpassword/${resetToken}`,
       userData
     );
     return response.data;
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
 
 // Get Login Status
 export const getLoginStatus = async () => {
   try {
-    const response = await axiosInstance.get(`${BACKEND_URL}/api/users/loggedin`);
+    const response = await axios.get(`${BACKEND_URL}/api/users/loggedin`);
     return response.data;
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
-
 // Get User Profile
 export const getUser = async () => {
   try {
-    const response = await axiosInstance.get(`${BACKEND_URL}/api/users/getuser`);
+    const response = await axios.get(`${BACKEND_URL}/api/users/getuser`);
     return response.data;
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
-
 // Update Profile
 export const updateUser = async (formData) => {
   try {
-    const response = await axiosInstance.patch(
+    const response = await axios.patch(
       `${BACKEND_URL}/api/users/updateuser`,
       formData
     );
     return response.data;
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
-
 // Update Password
 export const changePassword = async (formData) => {
   try {
-    const response = await axiosInstance.patch(
+    const response = await axios.patch(
       `${BACKEND_URL}/api/users/changepassword`,
       formData
     );
     return response.data;
   } catch (error) {
-    // The interceptor will handle the error toast
-    throw error;
+    const message =
+      (error.response && error.response.data && error.response.data.message) ||
+      error.message ||
+      error.toString();
+    toast.error(message);
   }
 };
