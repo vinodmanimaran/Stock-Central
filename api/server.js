f8d080648872c89c4f8ced34d5025d3f3e03b136
Vinod Kumar <117673132+vinodmanimaran@users.noreply.github.com>
2023-05-22 03:23:12 +0530

    Update server.js

--- api/server.js
+++ api/server.js
@@ -19,7 +19,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use (
   cors ({
-    origin: ['http://localhost:3000'],
+    origin: ['https://stockcentral.netlify.app/'],
     credentials: true,
   })
 );
