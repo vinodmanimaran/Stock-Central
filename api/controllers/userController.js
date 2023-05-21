638445282986e5085ab6d76b08ee4dd7fc686bfb
Vinod Kumar <117673132+vinodmanimaran@users.noreply.github.com>
2023-05-22 03:27:34 +0530

    Update userController.js

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -359,7 +359,7 @@ const sendPasswordResetEmail = asyncHandler (async (req, res) => {
   });
   await token.save ();
 
-  const resetUrl = `http://localhost:3000/reset/${token.token}`;
+  const resetUrl = `https://stockcentral.netlify.app/reset/${token.token}`;
   const message = `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\nPlease click on the following link, or paste this into your browser to complete the process:\n\n${resetUrl}\n\nIf you did not request this, please ignore this email and your password will remain unchanged.\n`;
 
   await sendEmail ({
